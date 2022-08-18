38308e3bd8924684e37683047dba8ae0507188c0
EduardoAzaldegui <81339919+EduardoAzaldegui@users.noreply.github.com>
2022-08-18 17:03:49 -0500

    as

--- prueba.js
+++
@@ -1,9 +0,0 @@
-fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
-    method: "post",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        mokepon: mascotaJugador
-    })
-})
